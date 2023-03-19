42a151359cf6fc69e32ffdf418fbadfcbc2e3fa5
DanilaRezcikovs <125744863+DanilaRezcikovs@users.noreply.github.com>
2023-03-19 19:33:53 +0000

    commit

--- build_heap.py
+++ build_heap.py
@@ -2,38 +2,56 @@
 
 
 def build_heap(data):
+    n = len(data)
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    for i in range(n//2, -1, -1):
+        swaps = heapify(data, i, swaps)
+    return swaps
 
-
+def heapify(data, i, swaps):
+    n = len(data)
+    min_idx = i
+    l = 2*i + 1
+    r = 2*i + 2
+    if l < n and data[l] < data[min_idx]:
+        min_idx = l
+    if r < n and data[r] < data[min_idx]:
+        min_idx = r
+    if i != min_idx:
+        data[i], data[min_idx] = data[min_idx], data[i]
+        swaps.append((i, min_idx))
+        swaps = heapify(data, min_idx, swaps)
     return swaps
 
 
+
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
     # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
+    # checks if length of data is the same as the said length
     assert len(data) == n
 
-    # calls function to assess the data 
+    # calls function to assess the data
     # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
+    # checks if the heap property is satisfied
+    for i in range(n//2):
+        if 2*i+1 < n and data[i] > data[2*i+1]:
+            print("ERROR: heap property not satisfied")
+            return
 
+        if 2*i+2 < n and data[i] > data[2*i+2]:
+            print("ERROR: heap property not satisfied")
+            return
+
+    # output how many swaps were made,
+    # this number should be less than 4n (less than 4*len(data))
+    print(len(swaps))
 
     # output all swaps
-    print(len(swaps))
     for i, j in swaps:
         print(i, j)
 
